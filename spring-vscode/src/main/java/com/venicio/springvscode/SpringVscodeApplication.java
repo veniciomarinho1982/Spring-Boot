79b9e9cc895b71982d3cd4ccb8cae97557ec3a02
Venicio.Marinho <venicio.marinho@assurant.com>
2023-07-03 13:32:24 -0300

    Primeiro commit Hello World

---
+++ spring-vscode/src/main/java/com/venicio/springvscode/SpringVscodeApplication.java
@@ -0,0 +1,13 @@
+package com.venicio.springvscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVscodeApplication.class, args);
+	}
+
+}
